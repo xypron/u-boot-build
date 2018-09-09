a3e7bffeab1e03d2d98097c7cec1706e56a07399
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-09 11:14:57 +0200

    Add u-boot-test
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

---
+++ u-boot-test/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export PATH=$(pwd):/usr/bin:/bin
+export PYTHONPATH=$(pwd)
+cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_selftest.py
+# cd ../denx && python3 ./test/py/test.py --bd=qemu-arm64 --build-dir=.
