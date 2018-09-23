b9e495af0ee7f6db4bf7c2ebddf94d433f9218cc
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-23 05:13:46 +0200

    Select also test_efi_loader.py
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/u_boot_boardenv_qemu_arm.py
+++ u-boot-test/u_boot_boardenv_qemu_arm.py
@@ -1,6 +1,6 @@
 env__net_dhcp_server = True
 env__efi_loader_helloworld_efi = {
-    "fn": "helloworld.efi",
-    "size": 4962,
-    "crc32": "8c45da68",
+    "fn": "lib/efi_loader/helloworld.efi",
+    "size": 2128,
+    "crc32": "facdd76b",
 }
