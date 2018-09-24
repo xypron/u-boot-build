2da95ac6a98f34c9601f0e6236265754a6859dc2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-24 19:43:58 +0200

    unittest should include efi_loader test
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
-cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_selftest.py
+cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_
 # cd ../denx && python3 ./test/py/test.py --bd=qemu-arm64 --build-dir=.
