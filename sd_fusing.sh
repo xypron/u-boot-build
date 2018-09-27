5568f13a2a1566782f6c0dc6003b1a7b0ad8eeb7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-27 07:49:13 +0200

    sd_fusing.sh: use sudo, remove invalid line
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- sd_fusing.sh
+++ sd_fusing.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo dd conv=fsync,notrunc if=u-boot.img of=$1 bs=1024 seek=32
-dd if=SPL of=$1 bs=1k seek=1
-dd if=u-boot.img of=$1 bs=1k seek=69
+sudo dd if=SPL of=$1 bs=1k seek=1
+sudo dd if=u-boot.img of=$1 bs=1k seek=69
 sudo eject $1
