a89dd06b1d1b58ab34509637fc692817f80d02c9
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-28 22:07:50 +0200

    sd_fusing: sync notrunc
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- sd_fusing.sh
+++ sd_fusing.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo dd if=SPL of=$1 bs=1k seek=1
-sudo dd if=u-boot.img of=$1 bs=1k seek=69
+sudo dd iflag=dsync oflag=dsync conv=notrunc if=SPL of=$1 bs=1k seek=1
+sudo dd iflag=dsync oflag=dsync conv=notrunc if=u-boot.img of=$1 bs=1k seek=69
 sudo eject $1
