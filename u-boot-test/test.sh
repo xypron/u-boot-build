29d382e4373aff4029570a42af92a27966c27cf9
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-30 10:11:20 +0000

    u-boot-test/test.sh: connect SD card, power down
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
+sd-mux-ctrl -v 0 -d
 cd ../denx && ./test/py/test.py --bd=orangepi_pc --build-dir=. # -k=test_efi_
+relay-card off
