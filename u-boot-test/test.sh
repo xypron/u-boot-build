b02be88a2ff3ffc0c7d8b771822428f6f742ec3d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-16 22:26:40 +0000

    Update config
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -2,4 +2,4 @@
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
 sd-mux-ctrl -v 0 -d
-cd ../denx && ./test/py/test.py --bd=pine64-lts --build-dir=. # -k=test_efi_
+cd ../denx && ./test/py/test.py --bd=pine64-lts --build-dir=. -k=test_efi_
