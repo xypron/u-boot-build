c1c63158b143dfe843637cbd96ba98e882a9d7bc
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-08-30 07:20:17 +0000

    u-boot-test: use -td argument for sd-mux-ctrl
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
-sd-mux-ctrl -v 0 -d
+sd-mux-ctrl -v 0 -td
 cd ../denx && ./test/py/test.py --bd=pine64-lts --build-dir=. -k=test_efi_
