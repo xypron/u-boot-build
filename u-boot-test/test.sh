7b1ca82daf9b96b0dc51d8139ae637f7f63fd170
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-10-27 00:07:37 +0200

    u-boot-test/test.sh: do not use Python3 yet
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -2,4 +2,4 @@
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
 # cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_
-cd ../denx && python3 ./test/py/test.py --bd=qemu-arm64 --build-dir=.
+cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=.
