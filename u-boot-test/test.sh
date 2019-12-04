4c072e72ce07b332ee222ecd3a3ae511ef104f41
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-12-04 22:50:53 +0100

    Testings: fix tftp
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
-# cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_
-cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=.
+cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=. -k=test_efi_
+#cd ../denx && ./test/py/test.py --bd=qemu-arm64 --build-dir=.
--- u-boot-test/u_boot_boardenv_qemu_arm64.py
+++ u-boot-test/u_boot_boardenv_qemu_arm64.py
@@ -1,6 +1,18 @@
 env__net_dhcp_server = True
-env__efi_loader_helloworld_efi = {
+env__net_static_env_vars = [
+    ('ipaddr', '10.0.2.15'),
+    ('netmask', '255.255.255.0'),
+    ('serverip', '10.0.2.2'),
+]
+env__net_tftp_readable_file = {
     "fn": "helloworld.efi",
-    "size": 4962,
-    "crc32": "8c45da68",
+    "size": 4480,
+    "crc32": "19f9c0ab",
+    "addr": 0x40400000,
+}
+env__efi_loader_helloworld_file = {
+    "fn": "helloworld.efi",
+    "size": 4480,
+    "crc32": "19f9c0ab",
+    "addr": 0x40400000,
 }
