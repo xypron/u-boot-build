382439f62b010f9d2508c246e0b2b25042d8d68f
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-12-29 10:43:57 +0100

    u_boot_boardenv_qemu_arm64.py
    Add file specification for test_efi_fit.py.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/u_boot_boardenv_qemu_arm64.py
+++ u-boot-test/u_boot_boardenv_qemu_arm64.py
@@ -1,18 +1,33 @@
 env__net_dhcp_server = True
+"""
 env__net_static_env_vars = [
     ('ipaddr', '10.0.2.15'),
     ('netmask', '255.255.255.0'),
     ('serverip', '10.0.2.2'),
 ]
+"""
 env__net_tftp_readable_file = {
     "fn": "helloworld.efi",
     "size": 4480,
     "crc32": "19f9c0ab",
     "addr": 0x40400000,
 }
+
+env__efi_loader_grub_file = {
+    "fn": "grubaa64.efi",
+    "size": 729088,
+    "crc32": "db781685",
+}
+
 env__efi_loader_helloworld_file = {
     "fn": "helloworld.efi",
     "size": 4480,
     "crc32": "19f9c0ab",
     "addr": 0x40400000,
+}
+
+env__efi_fit_tftp_file = {
+    "fn": "test-efi-fit.img",
+    "addr": 0x40400000,
+    "dn": "../tftp/",
 }
