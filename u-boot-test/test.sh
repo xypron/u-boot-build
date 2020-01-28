29b9637da294cc6efde510e5a84e023cffbc41b1
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-01-28 18:30:37 +0000

    u-boot-test/test.sh: show why tests are skipped
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -2,5 +2,5 @@
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
 sd-mux-ctrl -v 0 -d
-cd ../denx && ./test/py/test.py --bd=orangepi_pc --build-dir=. # -k=test_efi_
+cd ../denx && ./test/py/test.py -ra --bd=orangepi_pc --build-dir=. -k=test_efi_
 relay-card off
