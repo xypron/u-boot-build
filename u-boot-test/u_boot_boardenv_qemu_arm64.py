d5c822c78d81db1ffc162afb586736acdf43b8f7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-03-28 10:58:53 +0100

    u_boot_boardenv_qemu_arm64.py: enable network
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/u_boot_boardenv_qemu_arm64.py
+++ u-boot-test/u_boot_boardenv_qemu_arm64.py
@@ -1,11 +1,11 @@
 env__net_dhcp_server = True
-"""
+
 env__net_static_env_vars = [
     ('ipaddr', '10.0.2.15'),
     ('netmask', '255.255.255.0'),
     ('serverip', '10.0.2.2'),
 ]
-"""
+
 env__net_tftp_readable_file = {
     "fn": "helloworld.efi",
     "size": 4480,
