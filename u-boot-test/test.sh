6241c3f1d7b8cde43ac26725ab96aa7a1b27ecc4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-04-16 06:29:31 +0000

    test.sh: move relaycard off before sdmux
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- u-boot-test/test.sh
+++ u-boot-test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATH=$(pwd):/usr/bin:/bin
 export PYTHONPATH=$(pwd)
+relay-card off
 sd-mux-ctrl -v 0 -d
 cd ../denx && ./test/py/test.py -ra --bd=orangepi_pc --build-dir=. -k=test_efi_
-relay-card off
