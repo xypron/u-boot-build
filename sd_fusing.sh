585614b599183c6b83c83af1c203da7636c838b9
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-08-25 12:07:30 +0200

    chmod 755 sd_fusing.sh
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- sd_fusing.sh
+++ sd_fusing.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sudo dd conv=fsync,notrunc if=spl/u-boot-spl.bin of=$1 seek=34
-sudo dd conv=fsync,notruncif=u-boot.itb of=$1 seek=2082
+
+set -e
+
+sudo dd conv=fsync,notrunc if=u-boot-spl.bin of=$1 seek=34
+sudo dd conv=fsync,notrunc if=u-boot.itb of=$1 seek=2082
 sudo eject $1
