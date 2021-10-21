2bf2799f92c76e470f5bf78afcc2c35c65a652d0
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-10-21 01:32:04 +0000

    Update config
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- config/general.h
+++ config/general.h
@@ -4,6 +4,8 @@
 #define NTP_CMD			/* NTP commands */
 #define VLAN_CMD		/* VLAN commands */
 #define IMAGE_EFI		/* EFI image support */
+#define SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#define SANBOOT_PROTO_ISCSI	/* iSCSI SAN boot protocol */
 #define DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
 #define DOWNLOAD_PROTO_FTP	/* File Transfer Protocol */
 #define DOWNLOAD_PROTO_NFS	/* Network File System Protocol */
